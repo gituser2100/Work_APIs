8823ae296d2b67882a2ab4fb4895c809988fd8cb
gituser2100 <147813799+gituser2100@users.noreply.github.com>
2025-01-17 17:28:40 +0000

    adding condev-solutions

--- paid_checker/condev-solutions.js
+++ paid_checker/condev-solutions.js
@@ -1,4 +1,21 @@
-import cors from 'cors';
-import express from 'express';
+const express = require('express')
+const cors = require('cors')
 
-const app = express()
+const app = express();
+app.use(cors())
+
+app.use(express.json());
+
+const port = process.env.PORT || 10000
+
+app.listen(port, ()=> {
+    console.log(`Checker is running on port ${port}`)
+});
+
+
+app.get('/condev_solutions_paid', (req, res) => {
+    app.get('/check_paid', async(req, res) => {
+        console.log('paid checker request received')
+        res.json(true)
+    })
+})
